d3f29f277170c24e664024caae66b931af8a41c7
jacheg <jacchegia@gmail.com>
2019-12-12 02:25:20 +0000

    got further, I think its comparing the wrong values in the div. FIX ITTTT!!!!

--- js/script.js
+++ js/script.js
@@ -3,11 +3,15 @@ var gameOver = false;
 
 function performLogic(button, tile){
   counter++;
-
-  if((counter % 2) == 0){
-    $(tile).html("x");
-  } else{
-    $(tile).html("o");
+  if(gameOver == false){
+    if((counter % 2) == 0){
+      $(tile).text("x");
+      if(tile1 == tile2){
+        $("body").css("background-color", "yellow");
+      }
+    } else{
+      $(tile).text("o");
+    }
   }
 
   if(counter == 9){
@@ -17,6 +21,51 @@ function performLogic(button, tile){
   }
 }
 
+function check3(tile1, tile2, tile3){
+  if((tile1 == tile2) && (tile2 == tile3)){
+    return true;
+  }
+  return false;
+}
+
+function checkVerticalWin(){
+  if(check3(tile1, tile4, tile7)){
+    return true;
+    gameOver = true;
+    $("h1").html("x wins!!");
+  } else if(check3(tile2, tile5, tile8)){
+    return true;
+  } else if(check3(tile3, tile6, tile9)){
+    return true;
+  }
+  return false;
+}
+
+function checkHorizontalWin(){
+  if(check3(tile1, tile2, tile3)){
+    return true;
+  } else if(check3(tile4, tile5, tile6)){
+    return true;
+  } else if(check3(tile7, tile8, tile9)){
+    return true;
+  }
+}
+
+function checkDiagonalWin(){
+  if(check3(tile1, tile5, tile9)){
+    return true;
+  } else if(check3(tile7, tile5, tile3)){
+    return true;
+  } 
+return false;
+}
+
+// function checkAllWin(){
+//   if(checkVerticalWin() || checkHorizontalWin() || checkDiagonalWin){
+//     gameOver = true;
+//     $("h1").html("x wins!!");
+//   }
+// }
 
 
 $("#button1").click(function() {
